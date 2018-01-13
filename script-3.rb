366af3037f1c1aef05f75222dc1e66029d01351f
AirOne70 <finot.erwan@gmail.com>
2018-01-13 15:52:42 +0100

    Script: création récursive de l'index + affichage

--- script-3.rb
+++ script-3.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 require 'liquid'
 
 module Booxygen
-  VERSION = "0.3.1"
+  VERSION = "0.3.2"
 
   class Templates
     def initialize(dir)
@@ -14,7 +14,7 @@ module Booxygen
 
   class Doxygen
     def initialize
-      @compounds = []
+      @compounds = {}
     end
 
     def parse(directory)
@@ -25,92 +25,74 @@ module Booxygen
 
       xsd_first_element = xsd.at_xpath('//xsd:element')
 
-      # DoxygenType
-      xsd_complex_type = xsd.at_xpath('//xsd:complexType[@name=\'' + xsd_first_element['type'] + '\']')
+      @compounds = rec_parse(xsd_first_element,xsd,xml.at_xpath('//doxygenindex'))
+    end
 
-      # Elements et attributs de DoxygenType
-      xsd_elements_list = xsd_complex_type.xpath('xsd:sequence/xsd:element')
-      # xsd_attributs_list = xsd_complex_type.xpath('xsd:attribute')
+    def rec_parse(index, xsd_file, xml)
+      result = {}
 
+      xsd_complex_type = xsd_file.at_xpath("//xsd:complexType[@name='#{index['type']}']")
+
+      # Récupération des éléments et attributs
+      xsd_elements_list = xsd_complex_type.xpath('.//xsd:sequence/xsd:element')
+      xsd_attributs_list = xsd_complex_type.xpath('.//xsd:attribute')
+
+
+      # Traitement des attributs
+      xsd_attributs_list.each do |attribut|
+        result[attribut['name']] = xml[attribut['name']]
+      end
+
+
+      # Traitement des éléments
       xsd_elements_list.each do |xsd_element|
+        xml_elements_list = xml.xpath("./#{xsd_element['name']}")
 
-        # Code de chacun des éléments
-        xml_elements_list = xml.xpath('//doxygenindex/' + xsd_element['name'])
-
-        xml_elements_list.each do |xml_element_code|
+        xml_elements_list.each_with_index do |xml_element_code, index_nbr|
           element = {}
 
-          # Récupération des éléments et attributs associés au type
-          xsd_complex_type2 = xsd.at_xpath('//xsd:complexType[@name=\'' + xsd_element['type'] + '\']')
-
-          xsd_elements_list2 = xsd_complex_type2.xpath('xsd:sequence/xsd:element')
-          xsd_attributs_list2 = xsd_complex_type2.xpath('xsd:attribute')
-
-          # Traiter tous les attributs
-          xsd_attributs_list2.each do |attr2|
-            element[attr2['name']] = xml_element_code[attr2['name']]
-            puts attr2['name'] + ' : ' + element[attr2['name']]
+          if xsd_element['type'].start_with?('xsd:')
+            # Si c'est un type inconnu
+            element[xsd_element['name']] = xml_element_code
+          else
+            # Si c'est un type connu
+            element[xsd_element['name']] = rec_parse(xsd_element, xsd_file, xml_element_code)
           end
 
-          # Traiter tous les éléments du XSD
-          xsd_elements_list2.each do |xsd_element2|
-
-            # Code de chacun des éléments en XML
-            xml_elements_list2 = xml_element_code.xpath('./' + xsd_element2['name'])
-
-            sub_element = {}
-            xml_elements_list2.each do |xml_element_code2|
-
-              if xsd_element2['type'].start_with?('xsd:')
-                # Si c'est un type inconnu
-                sub_element[0] = xml_element_code2
-                puts xsd_element2['name'] + ' : ' + sub_element[0]
-
-              else
-                # Si c'est un type connu
-                # Récupération des éléments et attributs associés au type
-                xsd_complex_type3 = xsd.at_xpath('//xsd:complexType[@name=\'' + xsd_element2['type'] + '\']')
-
-                xsd_elements_list3 = xsd_complex_type3.xpath('xsd:sequence/xsd:element')
-                xsd_attributs_list3 = xsd_complex_type3.xpath('xsd:attribute')
-
-                # Traiter tous les attributs
-                xsd_attributs_list3.each do |attr3|
-                  sub_element[attr3['name']] = xml_element_code2[attr3['name']]
-                  puts attr3['name'] + ' : ' + sub_element[attr3['name']]
-                end
-
-                # Traiter tous les éléments
-                xsd_elements_list3.each do |element3|
-                  sub_element[element3['name']] = xml_element_code2.xpath(element3['name'])
-                  sub_element[element3['name']].each do |element3_to_print|
-                    puts element3['name'] + ' : ' + element3_to_print
-                  end
-                end
-              end
-            end
-
-            #element['subelement_list'].push(sub_element)
-
-            puts ' '
-          end
-          @compounds.push(element)
+          result[index_nbr] = element
         end
       end
 
+      result
     end
 
-    def print
-      #@compounds.each do |node|
-      #  puts node['kind'] + ' ' + node.at_xpath('name')
-      #end
+    def rec_print_res(content)
+      if content.is_a?(Array)
+        content.each do |value|
+          rec_print_res value
+        end
+      elsif content.is_a?(Hash)
+        content.each do |key, value|
+          print "#{key} : "
+          rec_print_res value
+        end
+      else
+        print content, "\n"
+      end
+    end
+
+    def print_res
+      @compounds.each do |node|
+        rec_print_res node
+        print "\n"
+      end
     end
   end
 
   def self.run(directory)
     dox = Doxygen.new
     dox.parse(directory)
-    dox.print
+    dox.print_res
   end
 end
 
