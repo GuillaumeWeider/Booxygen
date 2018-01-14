d6dd9e9b443558e57de206074246b81052f2dd60
AirOne70 <finot.erwan@gmail.com>
2018-01-14 16:14:40 +0100

    Script: traitement des sous-fichiers XML

--- script-3.rb
+++ script-3.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 require 'liquid'
 
 module Booxygen
-  VERSION = "0.3.2"
+  VERSION = "0.3.3"
 
   class Templates
     def initialize(dir)
@@ -15,65 +15,72 @@ module Booxygen
   class Doxygen
     def initialize
       @index_hash = {}
-      @compounds_hash = {}
     end
 
-    # def parse_compound(ref_file)
-    #   xml = Nokogiri::XML(File.open($dir + ref_file + '.xml'))
-    #
-    #   xsd_name = xml.at_xpath('//doxygen')['xsi:noNamespaceSchemaLocation']
-    #   xsd = Nokogiri::XML(File.open($dir + xsd_name))
-    #
-    #   xsd_doxygen_element = xsd.at_xpath('//xsd:element[@name=\'doxygen\']')
-    #
-    #   @compounds_hash = rec_parse(xsd_doxygen_element, xsd, xml.at_xpath('//doxygenindex'))
-    #
-    # end
+    # Fonction de traitement des différents fichiers XML
+    def parse_compound(ref_file)
+      xml = Nokogiri::XML(File.open($dir + ref_file + '.xml'))
 
+      xsd_name = xml.at_xpath('//doxygen')['xsi:noNamespaceSchemaLocation']
+      xsd = Nokogiri::XML(File.open($dir + xsd_name))
+
+      xsd_doxygen_element = xsd.at_xpath('//xsd:element[@name=\'doxygen\']')
+
+      rec_parse(xsd_doxygen_element, xsd, xml.at_xpath('//doxygen'))
+    end
+
+    # Fonction de traitement d'un type donné dans un code XML donné
     def rec_parse(index, xsd, xml)
       result = {}
 
+      # Recherche des caractéristiques du type complexe donné
       xsd_complex_type = xsd.at_xpath("//xsd:complexType[@name='#{index['type']}']")
 
       # Récupération des éléments et attributs
       xsd_elements_list = xsd_complex_type.xpath('.//xsd:element')
       xsd_attributs_list = xsd_complex_type.xpath('.//xsd:attribute')
+      #xsd_group_list = xsd_complex_type.xpath('.//xsd:group')
 
+      # S'il n'y a aucun élément et aucun attribut
+      if xsd_elements_list.all? {|x| x.nil?} && xsd_attributs_list.all? {|x| x.nil?}
+        result['end'] = xml
+      else
 
-      # Traitement des attributs
-      xsd_attributs_list.each do |attribut|
-        result[attribut['name']] = xml[attribut['name']]
+        # Traitement des attributs
+        xsd_attributs_list.each do |attribut|
+          result[attribut['name']] = xml[attribut['name']]
 
-        # if attribut['type'] == 'CompoundKind'
-        #   result['subelement'] = parse_compound xml['refid']
-        # end
-      end
-
-
-      # Traitement des éléments
-      count = 0
-      xsd_elements_list.each do |xsd_element|
-        xml_elements_list = xml.xpath("./#{xsd_element['name']}")
-
-        xml_elements_list.each do |xml_element_code|
-          element = {}
-
-          if xsd_element['type'].start_with?('xsd:')
-            # Si c'est un type inconnu
-            element[xsd_element['name']] = xml_element_code
-          else
-            # Si c'est un type connu
-            element[xsd_element['name']] = rec_parse(xsd_element, xsd, xml_element_code)
+          if attribut['type'] == 'CompoundKind'
+            result['subelement'] = parse_compound(xml['refid'])
           end
+        end
 
-          result[count] = element
-          count += 1
+
+        # Traitement des éléments
+        count = 0
+        xsd_elements_list.each do |xsd_element|
+          xml_elements_list = xml.xpath("./#{xsd_element['name']}")
+
+          xml_elements_list.each do |xml_element_code|
+            element = {}
+
+            if xsd_element['type'].nil? || xsd_element['type'].start_with?('xsd:')
+              # Si c'est un type inconnu ou nul
+              element[xsd_element['name']] = xml_element_code
+            else
+              # Si c'est un type connu
+              element[xsd_element['name']] = rec_parse(xsd_element, xsd, xml_element_code)
+            end
+
+            result[count] = element
+            count += 1
+          end
         end
       end
-
       result
     end
 
+    # Fonction de traitement d'un fichier d'index
     def parse(xml_filename)
       xml = Nokogiri::XML(File.open($dir + xml_filename + '.xml'))
 
@@ -83,25 +90,28 @@ module Booxygen
       xsd_doxygen_element = xsd.at_xpath('//xsd:element[@name=\'doxygenindex\']')
 
       @index_hash = rec_parse(xsd_doxygen_element, xsd, xml.at_xpath('//doxygenindex'))
-
     end
 
+    # Fonction d'affichage selon le type de la donnée
     def rec_print_res(content)
-
       if content.is_a?(Array)
+        # Affichage du contenu d'un tableau
         content.each do |value|
           rec_print_res value
         end
       elsif content.is_a?(Hash)
+        # Affichage du contenu d'un hash (clé et valeur)
         content.each do |key, value|
           print "#{key} : "
           rec_print_res value
         end
       else
+        # Affichage du texte
         print content, "\n"
       end
     end
 
+    # Fonction d'affichage
     def print_res
       @index_hash.each do |node|
         # rec_print_res node
