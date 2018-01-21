4525a49be709a3c530f6fb88fb5c89661ec48264
Erwan Finot <finot.erwan@gmail.com>
2018-01-21 23:52:00 +0100

    Petite simplification du script

--- scripts/script-3.rb
+++ scripts/script-3.rb
@@ -4,7 +4,6 @@ require 'nokogiri'
 require 'liquid'
 
 require 'io/console'
-require 'awesome_print'
 
 module Booxygen
   VERSION = "0.3.3"
@@ -22,62 +21,52 @@ module Booxygen
       @types = {}
     end
 
-    # Fonction de traitement des différents fichiers XML
-    def parse_compound(ref_file)
-      xml = Nokogiri::XML(File.open("#{$dir}#{ref_file}.xml"))
-
-      xsd_name = xml.at_xpath('//doxygen')['xsi:noNamespaceSchemaLocation']
-      xsd = Nokogiri::XML(File.open("#{$dir}#{xsd_name}"))
-
-      xsd_doxygen_element = xsd.at_xpath('//xsd:element[@name=\'doxygen\']')
-
-      rec_parse(xsd_doxygen_element, xsd, xsd_name, xml.at_xpath('//doxygen'))
-    end
-
-    # Fonction de traitement d'un type donné dans un code XML donné
+    # Fonction de traitement d'un type donné, dans un code XML donné
     def rec_parse(index, xsd, xsd_name, xml)
       result = {}
 
+      # Si ce XSD n'a jamais été traité auparavant
       unless @types.has_key? xsd_name
         @types[xsd_name] = {}
       end
 
-      # Si ce type complexe a déjà été parsé
+      # Si ce type complexe n'a pas encore été parsé
       unless @types[xsd_name].has_key? index['type']
         # Recherche des caractéristiques du type complexe donné
         xsd_complex_type = xsd.at_xpath("//xsd:complexType[@name='#{index['type']}']")
 
-        # Récupération des éléments et attributs
+        # Récupération des éléments, attributs et simpleContent
         @types[xsd_name][index['type']] = {}
         @types[xsd_name][index['type']]['xsd_elements_list'] = xsd_complex_type.xpath('.//xsd:element')
         @types[xsd_name][index['type']]['xsd_attributes_list'] = xsd_complex_type.xpath('.//xsd:attribute')
         @types[xsd_name][index['type']]['xsd_simplecontent_list'] = xsd_complex_type.xpath('.//xsd:simpleContent')
       end
 
-      # S'il n'y a aucun élément et aucun attribut
+      # S'il n'y a aucun élément et aucun attribut, retourner l'ensemble du contenu
       if @types[xsd_name][index['type']]['xsd_elements_list'].all? {|x| x.nil?} && @types[xsd_name][index['type']]['xsd_attributes_list'].all? {|x| x.nil?}
         return xml.to_s
       else
-
         # Traitement des attributs
         @types[xsd_name][index['type']]['xsd_attributes_list'].each do |attribut|
+          # Récupération du code XML correspondant
           result[attribut['name']] = xml[attribut['name']]
 
           # Si c'est un type "CompoundKind", traiter son XML correspondant
           if attribut['type'] == 'CompoundKind'
-            result['sub_xml'] = parse_compound(xml['refid'])
+            result['sub_xml'] = parse(xml['refid'], false)
           end
         end
 
         # Traitement des éléments
         @types[xsd_name][index['type']]['xsd_elements_list'].each do |xsd_element|
-          xml_elements_list = xml.xpath("./#{xsd_element['name']}")
           element_array = []
 
-          xml_elements_list.each do |xml_element_code|
+          # Récupération du code XML correspondant
+          xml_elements_list = xml.xpath("./#{xsd_element['name']}")
 
+          xml_elements_list.each do |xml_element_code|
             if xsd_element['type'].nil? || xsd_element['type'].start_with?('xsd:')
-              # Si c'est un type inconnu ou nul
+              # Si c'est un type inconnu ou qu'il n'y a pas de type
               element_array.push xml_element_code.content.to_s
             else
               # Si c'est un type connu
@@ -96,20 +85,33 @@ module Booxygen
       result
     end
 
-    # Fonction de traitement d'un fichier d'index
-    def parse(xml_filename)
+    # Fonction de traitement d'un fichier XML
+    def parse(xml_filename, is_index)
       xml = Nokogiri::XML(File.open("#{$dir}#{xml_filename}.xml"))
 
-      xsd_name = xml.at_xpath('//doxygenindex')['xsi:noNamespaceSchemaLocation']
+      # Si le fichier correspond au fichier d'index
+      if is_index
+        xsd_first_element = 'doxygenindex'
+      else
+        xsd_first_element = 'doxygen'
+      end
+
+      xsd_name = xml.at_xpath("//#{xsd_first_element}")['xsi:noNamespaceSchemaLocation']
       xsd = Nokogiri::XML(File.open("#{$dir}#{xsd_name}"))
 
-      xsd_doxygen_element = xsd.at_xpath('//xsd:element[@name=\'doxygenindex\']')
+      xsd_doxygen_element = xsd.at_xpath("//xsd:element[@name='#{xsd_first_element}']")
 
-      @index_hash = rec_parse(xsd_doxygen_element, xsd, xsd_name, xml.at_xpath('//doxygenindex'))
+      res = rec_parse(xsd_doxygen_element, xsd, xsd_name, xml.at_xpath("//#{xsd_first_element}"))
+
+      if is_index
+        @index_hash = res
+      else
+        res
+      end
     end
 
-    # Fonction d'affichage
-    def print_res
+    # Fonction de génération des fichiers HTML
+    def generate_html
       compounds = []
 
       @index_hash['compound'].each do |value|
@@ -117,15 +119,15 @@ module Booxygen
       end
 
       loop do
-        template = Liquid::Template.parse(File.read('../templates/template1.liquid'))
-        File.write('../output/index.html', template.render('compounds' => compounds))
-
+        # Main page
         template = Liquid::Template.parse(File.read('../templates/main.liquid'))
         File.write('../output/html/main.html', template.render('compounds' => compounds))
 
+        # Index
         template = Liquid::Template.parse(File.read('../templates/classes.liquid'))
         File.write('../output/html/classes.html', template.render('compounds' => compounds))
 
+        # Classes
         compounds.each do |compound|
           if compound['kind'] == 'class'
             template = Liquid::Template.parse(File.read('../templates/class.liquid'))
@@ -133,20 +135,21 @@ module Booxygen
           end
         end
 
+        # ?
+
         print "\n", "Press any key to reload Liquid, or 'q' to quit."
         if STDIN.getch == "q"
           print "\n"
           break
         end
-
       end
     end
   end
 
   def self.run
     dox = Doxygen.new
-    dox.parse 'index'
-    dox.print_res
+    dox.parse('index', true)
+    dox.generate_html
   end
 end
 
